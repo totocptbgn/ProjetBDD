02ce086c717b2e9d622114c9f34c0808309763d1
Hind Hamila <44473242+hindhamila@users.noreply.github.com>
2018-11-29 16:25:34 +0100

    Update arch.sql

--- arch.sql
+++ arch.sql
@@ -8,29 +8,20 @@ DROP TABLE IF EXISTS Pays CASCADE;
 CREATE TABLE Athlete (
 	IDAthlete serial primary key,
 	NomAthlete text not null,
-	IDpays integer,
+	Pays text not null,
 	NbrMedailles integer,
-	Discipline text,
-	PRIMARY KEY (IDAthlete),
-	FOREIGN KEY (IDpays) REFERENCES Pays(IDpays)
+	Sport text not null,
+	Age int,
+	sexe text not null,
+	PRIMARY KEY (IDAthlete)
 );
 CREATE TABLE Equipe (
-	IDpays integer,
-	Discipline text not null,
-	Score integer,
 	IDequipe serial primary key,
-	PRIMARY KEY (IDequipe),
-	FOREIGN KEY (IDpays) REFERENCES Pays(IDpays)
+	Pays text not null,
+	Sport text not null,
+	PRIMARY KEY (IDequipe)
 );
-CREATE TABLE Pays (
-	IDpays serial primary key,
-	NbMedaillesOr int,
-	nbMedaillesArgent int,
-	nbMedaillesBronze int,
-	NomPays text not null,
-	Rang int,
-	PRIMARY KEY (IDpays)
-);
+
 CREATE TABLE Sport (
 	type text not null,
 	nomSport text not null,
@@ -53,11 +44,22 @@ CREATE TABLE EpreuveCollective (
 	IDSport int,
 	nomEpreuve text not null,
 	IDEpreuve serial primary key,
-	IDEquipeGagnante int,
-	IDEquipePerdante int,
+	IDEquipeGagnanteOr int,
+	IDEquipeGagnanteArgent int,
+	IDEquipeGagnanteBronze int,
 	dateEpreuve date,
 	PRIMARY KEY (IDEpreuve),
 	FOREIGN KEY (IDEquipeGagnante,IDEquipePerdante) REFERENCES Equipe(IDequipe)
+);
+CREATE TABLE Match (
+	NomMatch text not null,
+	IDMatch serial primary key,
+	IDEpreuve int,
+	IDGagnant int,
+	IDPerdant int,
+	PRIMARY KEY (IDMatch),
+	FOREIGN KEY (IDEpreuve) REFERENCES EpreuveCollective(IDEpreuve),
+	FOREIGN KEY (IDEpreuve) REFERENCES EpreuveIndividuel(IDEpreuve)
 );
 CREATE TABLE Medaille (
 	type text not null,
@@ -67,19 +69,5 @@ CREATE TABLE Medaille (
 	FOREIGN KEY (IDAthlete) REFERENCES Athlete(IDAthlete),
 	FOREIGN KEY (IDAthlete) REFERENCES Equipe(IDequipe)
 );
-CREATE TABLE Epreuve_Athlete (
-	IDAthlete int,
-	IDEpreuve int,
-	temps int,
-	score int,
-	FOREIGN KEY (IDAthlete) REFERENCES Athlete(IDAthlete),
-	FOREIGN KEY (IDEpreuve) REFERENCES Epreuve(IDEpreuve)
-);
-CREATE TABLE Epreuve_Equipe (
-	IDequipe int,
-	IDEpreuve int,
-	temps int,
-	score int,
-	FOREIGN KEY (IDequipe) REFERENCES equipe(IDequipe),
-	FOREIGN KEY (IDEpreuve) REFERENCES Epreuve(IDEpreuve)
+CREATE TABLE Participation (
 );
