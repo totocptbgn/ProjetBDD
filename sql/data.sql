b6d30be7f49ff391355dcd110400fb50630af3bf
Hind <hindhamila@gmail.com>
2018-12-01 17:48:28 +0100

    commit

--- sql/data.sql
+++ sql/data.sql
@@ -1,3 +1,31 @@
 /*
 Ce fichier est à utiliser pour remplir les tables crées avec 'arch.sql'.
 */
+
+INSERT INTO Sport (
+  ("Individuel","Natation"),
+  ("Individuel","Equitation"),
+  ("Individuel","Athlétisme"),
+  ("Collectif","Athlétisme"),
+  ("Individuel","Gymnastique"),
+  ("Collectif","Aviron"),
+  ("Collectif","Badminton"),
+  ("Collectif","Baseball"),
+  ("Collectif","Basketball"),
+  ("Individuel","Judo"),
+  ("Individuel","Escrime"),
+  ("Collectif","Escrime"),
+  ("Individuel","Natation"),
+  ("Collectif","Tennis"),
+  ("Individuel","Tennis"),
+  ("Collectif","Escrime"),
+  ("Individuel","Natation"),
+  ("Collectif","Tennis de table"),
+  ("Individuel","Tennis de table"),
+  ("Individuel","tir à l'arc"),
+  ("Individuel","volley ball")
+);
+
+INSERT INTO Athlete (
+  ()
+);
