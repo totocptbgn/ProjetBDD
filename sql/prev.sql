d5f60f5e2edfebf3fb0a0ebc0d72ef88978b2f4f
hindhamila <hindhamila@gmail.com>
2018-12-15 17:41:01 +0100

    ok

--- sql/prev.sql
+++ sql/prev.sql
@@ -1,1 +1,6 @@
+ALTER TABLE MatchIndividuel ADD Lieu varchar;
+ALTER TABLE MatchIndividuel ADD nbrVolontaires int;
+ALTER TABLE MatchCollectif ADD Lieu varchar;
+ALTER TABLE MatchCollectif ADD nbrVolontaires int;
 
+INSERT INTO MatchIndividuel (Lieu,nbrVolontaires) VALUES
